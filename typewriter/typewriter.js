0da1c24371a6f989d9b3d962045befe6172a5f37
Hadley <sutherlandhadley@gmail.com>
2022-11-22 10:30:07 -0700

    type

---
+++ typewriter/typewriter.js
@@ -0,0 +1,7 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0
+for (const char of sentence) {setTimeout(() => {
+  process.stdout.write(char);}, delay)
+  delay += 100
+}
+setTimeout(() => {console.log('\nline');},delay+1)
